af59517be4bc76828cd3b13a416900eff23a06fd
FidelDhlamini <goodkidfidel@gmail.com>
2019-12-11 14:41:41 +0200

    Added string for getting only south african locations

--- public/h2o.js
+++ public/h2o.js
@@ -2,11 +2,12 @@ function GetWeather(){
     var apiKey =  "&APPID=3a9bb3a0c565cfcb9993d0673045fb83";
     var api = "http://api.openweathermap.org/data/2.5/weather?q=";
     var units = "&units=metric";
+    var southAfrica = ",za";
      city = "";
 
     function whichCity(city){
        
-        var showWeather = api + city + apiKey + units; 
+        var showWeather = api + city + southAfrica + apiKey + units; 
         
         return axios.get(showWeather).then(results => results);
 
