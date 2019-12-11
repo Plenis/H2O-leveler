dd9e24383b258232c75991b8cba50ff950b66514
FidelDhlamini <goodkidfidel@gmail.com>
2019-12-11 12:57:16 +0200

    Added DOM logic for getting weather for the typed city

--- public/h2o.js
+++ public/h2o.js
@@ -1,13 +1,14 @@
 function GetWeather(){
-    var city = "";
     var apiKey =  "&APPID=3a9bb3a0c565cfcb9993d0673045fb83";
-    var api = "http://api.openweathermap.org/data/2.5/weather?";
+    var api = "http://api.openweathermap.org/data/2.5/weather?q=";
     var units = "&units=metric";
+     city = "";
 
     function whichCity(city){
        
         var showWeather = api + city + apiKey + units; 
-        return showWeather;
+        
+        return axios.get(showWeather).then(results => results);
 
     }
      console.log(whichCity("Johannesburg"));
@@ -16,6 +17,6 @@ function GetWeather(){
         whichCity
     }
 }
-console.log(GetWeather().whichCity("Johannesburg"))
+// console.log(GetWeather().whichCity("Johannesburg"))
 // http://api.openweathermap.org/data/2.5/weather?q=London&APPID=3a9bb3a0c565cfcb9993d0673045fb83&units=metric
 
