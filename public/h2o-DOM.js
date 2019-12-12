362b0ec0f43c166e25a0378ff719bf2bd3a4f354
FidelDhlamini <goodkidfidel@gmail.com>
2019-12-12 10:28:40 +0200

    added object keys for temperature

--- public/h2o-DOM.js
+++ public/h2o-DOM.js
@@ -1,3 +1,5 @@
+// import { request } from "http";
+
 const textbox = document.querySelector(".cityInput")
 const sbmtButton = document.querySelector(".submitButton")
 const display = document.querySelector(".weatherDisplay")
@@ -5,22 +7,17 @@ const mati = GetWeather();
 
 
 
-  // const colorsElem = document.querySelector(".brands");
-  // colorsElem.innerHTML = makesTemplate({
-  //   brands: response.data
-  // });
+  
 
 sbmtButton.addEventListener("click",function(){
   let urlString = mati.whichCity(textbox.value);
   urlString.then(function(response) {
-  // handle success
-  console.log(response.data.main.temp);
- textbox.innerHTML = console.log(response.data.main.temp);
+    if(response.data === "404 (Not Found)"){
+      display.innerHTML = "Enter a South African location"
+    }
+ 
   display.innerHTML = response.data.main.temp;
-  // const colorsElem = document.querySelector(".brands");
-  // colorsElem.innerHTML = makesTemplate({
-  //   brands: response.data
-  // });
+ 
 });
  
 
